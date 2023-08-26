579bdb122f20894c362a477508a20037c7c355ab
Kevin Anderson <kevinand@protonmail.com>
2023-08-26 11:55:40 -0500

    Reorg examples

--- src/examples/position.lisp
+++ src/examples/position.lisp
@@ -1,9 +1,7 @@
-(defpackage :alex/examples
+(defpackage :alex/position
   (:use :cl))
 
-(in-package :alex/examples)
-
-;; Binary file is in little endian format.
+(in-package :alex/position)
 
 (defparameter *000-audio-input-red-left-mono-row-0-column-0*
   '(:PATCH-SIZE-IN-BYTES 96
@@ -15,35 +13,24 @@
      0 0 0 0
      0 0 0 0
      3 0 0 0
-     ;; Row/Column position, can be 0-39 for a grid of 8x5
-     0 0 0 0
-
+     ;; Position, can be anything from 0 to 39
      0 0 0 0
      0 0 0 0
-
-     ;; Input
-     ;; 0 - Stereo
-     ;; 1 - Left
-     ;; 2 - Right
+     0 0 0 0
      1 0 0 0
-
      0 0 0 0
      0 0 0 0
      0 0 0 0
      0 0 0 0
      0 0 0 0
 
-     ;; Looks like this block of null bytes is padding between the
-     ;; modules and the connections/colors
      0 0 0 0
      0 0 0 0
      0 0 0 0
 
-     ;; Colors
-     ;; 3 - Red
      3 0 0 0)))
 
-(defparameter *000-two-audio-input-red-left-mono-row-00-col-00-row00-col-01*
+(defparameter *000-two-audio-input-red-left-mono-row-00-col-00-row01-col-00*
   '(:PATCH-SIZE-IN-BYTES 156
     :PATCH-NAME ""
     :PATCH-NUMBER-OF-MODULES 2
@@ -68,7 +55,7 @@
      0 0 0 0
      0 0 0 0
      3 0 0 0
-     1 0 0 0
+     8 0 0 0
      0 0 0 0
      0 0 0 0
      1 0 0 0
@@ -85,65 +72,7 @@
      3 0 0 0
      3 0 0 0)))
 
-(defparameter *000-three-audio-input-red-left-mono-row-00-col-00-row-00-col-01-row-00-col-02*
-  '(:PATCH-SIZE-IN-BYTES 216
-   :PATCH-NAME ""
-   :PATCH-NUMBER-OF-MODULES 3
-   :PATCH-BODY
-    (14 0 0 0
-     1  0 0 0
-     0 0 0 0
-     0 0 0 0
-     3 0 0 0
-     0 0 0 0
-     0 0 0 0
-     0 0 0 0
-     1 0 0 0
-     0 0 0 0
-     0 0 0 0
-     0 0 0 0
-     0 0 0 0
-     0 0 0 0
-
-     14 0 0 0
-     1 0 0 0
-     0 0 0 0
-     0 0 0 0
-     3 0 0 0
-     1 0 0 0
-     0 0 0 0
-     0 0 0 0
-     1 0 0 0
-     0 0 0 0
-     0 0 0 0
-     0 0 0 0
-     0 0 0 0
-     0 0 0 0
-
-     14 0 0 0
-     1 0 0 0
-     0 0 0 0
-     0 0 0 0
-     3 0 0 0
-     2 0 0 0
-     0 0 0 0
-     0 0 0 0
-     1 0 0 0
-     0 0 0 0
-     0 0 0 0
-     0 0 0 0
-     0 0 0 0
-     0 0 0 0
-
-     0 0 0 0
-     0 0 0 0
-     0 0 0 0
-
-     3 0 0 0
-     3 0 0 0
-     3 0 0 0)))
-
-(defparameter *000-audio-input-red-left-mono-row-0-column-0-audio-output-red-left-mono-row-0-column-1*
+(defparameter *000-two-audio-input-red-left-mono-row-00-col-00-row04-col-07*
   '(:PATCH-SIZE-IN-BYTES 156
     :PATCH-NAME ""
     :PATCH-NUMBER-OF-MODULES 2
@@ -164,14 +93,14 @@
      0 0 0 0
 
      14 0 0 0
-     2 0 0 0
-     0 0 0 0
-     0 0 0 0
-     3 0 0 0
      1 0 0 0
      0 0 0 0
      0 0 0 0
-     0 1 0 0
+     3 0 0 0
+     39 0 0 0
+     0 0 0 0
+     0 0 0 0
+     1 0 0 0
      0 0 0 0
      0 0 0 0
      0 0 0 0
